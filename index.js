1f5e147cb5fd0407676b1e129c2dea512b2263cd
NjugunaMalcom <malcomsammy18@gmail.com>
2023-12-14 10:11:42 -0800

    bonus added

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function backG(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
 function newImage(image, left, bottom){
     let object = document.createElement('img')
     object.src = image
@@ -14,6 +21,12 @@ function newItem(image, left, bottom){
         object.remove()
     })
 }
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+backG('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+backG('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
 
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
